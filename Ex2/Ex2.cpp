a8ad2ac2dda552cdd6f0596ab2c5a4b8a85bb0ce
GMorelle-2412 <geoffreymorelle2412@gmail.com>
2026-03-03 14:21:02 +0100

    open

--- Ex2/Ex2.cpp
+++ Ex2/Ex2.cpp
@@ -1,5 +1,14 @@
+/*Question nº2
+Soit adr_deb un pointeur sur entiers, vous ferez en sorte que adr_deb pointe sur un 
+emplacement mémoire suffisant afin de stocker 6 entiers donnés par l’utilisateur. 
+Puis vous rangerez ces valeurs dans l’ordre croissant.
+Rechercher le maximum, l'afficher ainsi que son adresse et sa position.*/
+
 #include <stdio.h>
+#include <stdlib.h>
+
+int*adr_deb =(int*)malloc(sizeof(int));
 
 int main(){
-
+	print("", adr_deb);
 }
